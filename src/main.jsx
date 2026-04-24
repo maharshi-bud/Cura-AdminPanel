b97e52786ba6c6456fda19c315ef3ed90ae8274f
maharshi-bud <maharshi.budtech@gmail.com>
2026-04-24 12:59:27 +0530

    Railwya

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { store, persistor } from "./store/store";
+import { store, persistor } from "./Store/Store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
